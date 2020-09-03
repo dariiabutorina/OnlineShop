9517b78d3b72b23e337975b3cac9c18d7efccec7
dariiapikul <dariyapikul@gmail.com>
2020-09-03 18:53:12 +0300

    MainApp : update method usage modified

--- src/main/java/com/internet/shop/MainApp.java
+++ src/main/java/com/internet/shop/MainApp.java
@@ -9,25 +9,39 @@ public class MainApp {
     private static final Injector INJECTOR = Injector.getInstance("com.internet.shop");
 
     public static void main(String[] args) {
+        System.out.println("Creating two new products.");
         ProductService productService = (ProductService) INJECTOR.getInstance(ProductService.class);
         Product diorProduct = new Product("Dior Backstage Foundation",
                 BigDecimal.valueOf(2000.0));
-        productService.create(diorProduct);
         Product limecrimeProduct = new Product("LIMECRIME Nude Eyeshadow Palette",
                 BigDecimal.valueOf(700.0));
+        productService.create(diorProduct);
         productService.create(limecrimeProduct);
         System.out.println("All products:");
-        for (Product product : productService.getAll()) {
-            System.out.println(product);
-        }
-        System.out.println("Changing price of Dior Foundation:");
-        diorProduct.setPrice(diorProduct.getPrice().multiply(BigDecimal.valueOf(0.9)));
-        productService.update(diorProduct);
-        System.out.println(limecrimeProduct);
-        System.out.println("Deleting LIMECRIME Eyeshadow Palette:");
+        productService
+                .getAll()
+                .forEach(System.out::println);
+
+        System.out.println("\nChanging price of Dior Foundation.");
+        Product diorProductNew = new Product("Dior Backstage Foundation",
+                BigDecimal.valueOf(2000.0)
+                        .multiply(BigDecimal.valueOf(0.9)));
+        diorProductNew.setId(diorProduct.getId());
+        System.out.println("New Dior product:\n"
+                + diorProductNew);
+        productService.update(diorProductNew);
+        System.out.println("All products:");
+        productService
+                .getAll()
+                .forEach(System.out::println);
+
+        System.out.println("\nDeleting LIMECRIME Eyeshadow Palette."
+                + "\nLIMECRIME product:\n"
+                + limecrimeProduct);
         productService.deleteById(limecrimeProduct.getId());
-        for (Product product : productService.getAll()) {
-            System.out.println(product);
-        }
+        System.out.println("All products:");
+        productService
+                .getAll()
+                .forEach(System.out::println);
     }
 }
