037d496230a4ca1a5fcd675ac1d516503c0387c9
dariiapikul <dariyapikul@gmail.com>
2020-09-10 15:41:57 +0300

    Fix conflicts

--- src/main/java/com/internet/shop/controller/IndexController.java
+++ src/main/java/com/internet/shop/controller/IndexController.java
@@ -6,10 +6,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class StartController extends HttpServlet {
+public class IndexController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/views/start.jsp").forward(req, resp);
+        req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
     }
 }
