03d9ce684087d8dc4e567be86e25332c61359750
dariiapikul <dariyapikul@gmail.com>
2020-09-16 23:44:30 +0300

    AuthorizationFilter : List -> Set

--- src/main/java/com/internet/shop/web/filter/AuthorizationFilter.java
+++ src/main/java/com/internet/shop/web/filter/AuthorizationFilter.java
@@ -8,8 +8,8 @@ import com.internet.shop.model.User;
 import com.internet.shop.service.interfaces.UserService;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -21,9 +21,11 @@ import javax.servlet.http.HttpServletResponse;
 
 public class AuthorizationFilter implements Filter {
     private static final Injector injector = Injector.getInstance("com.internet.shop");
+    public static final Set<Role.RoleName> USER = Set.of(Role.RoleName.USER);
+    public static final Set<Role.RoleName> ADMIN = Set.of(Role.RoleName.ADMIN);
     private final UserService userService =
             (UserService) injector.getInstance(UserService.class);
-    private Map<String, List<Role.RoleName>> protectedUrls;
+    private Map<String, Set<Role.RoleName>> protectedUrls;
 
     @Override
     public void init(FilterConfig filterConfig) {
@@ -31,22 +33,22 @@ public class AuthorizationFilter implements Filter {
         /*
         Users only
         */
-        protectedUrls.put("/product/buy", List.of(Role.RoleName.USER));
-        protectedUrls.put("/shopping-cart", List.of(Role.RoleName.USER));
-        protectedUrls.put("/shopping-cart/delete", List.of(Role.RoleName.USER));
-        protectedUrls.put("/order/complete", List.of(Role.RoleName.USER));
+        protectedUrls.put("/product/buy", USER);
+        protectedUrls.put("/shopping-cart", USER);
+        protectedUrls.put("/shopping-cart/delete", USER);
+        protectedUrls.put("/order/complete", USER);
         /*
         Admins only
         */
-        protectedUrls.put("/users/all", List.of(Role.RoleName.ADMIN));
-        protectedUrls.put("/user/delete", List.of(Role.RoleName.ADMIN));
-        protectedUrls.put("/products/all/admin", List.of(Role.RoleName.ADMIN));
-        protectedUrls.put("/product/delete", List.of(Role.RoleName.ADMIN));
-        protectedUrls.put("/product/add", List.of(Role.RoleName.ADMIN));
-        protectedUrls.put("/orders/all", List.of(Role.RoleName.ADMIN));
-        protectedUrls.put("/order/delete", List.of(Role.RoleName.ADMIN));
-        protectedUrls.put("/user/details/admin", List.of(Role.RoleName.ADMIN));
-        protectedUrls.put("/user/role", List.of(Role.RoleName.ADMIN));
+        protectedUrls.put("/users/all", ADMIN);
+        protectedUrls.put("/user/delete", ADMIN);
+        protectedUrls.put("/products/all/admin", ADMIN);
+        protectedUrls.put("/product/delete", ADMIN);
+        protectedUrls.put("/product/add", ADMIN);
+        protectedUrls.put("/orders/all", ADMIN);
+        protectedUrls.put("/order/delete", ADMIN);
+        protectedUrls.put("/user/details/admin", ADMIN);
+        protectedUrls.put("/user/role", ADMIN);
     }
 
     @Override
@@ -69,7 +71,7 @@ public class AuthorizationFilter implements Filter {
     public void destroy() {
     }
 
-    private static boolean isAuthorized(User user, List<Role.RoleName> authorizedRoles) {
+    private static boolean isAuthorized(User user, Set<Role.RoleName> authorizedRoles) {
         return user.getRoles().stream()
                 .map(Role::getRoleName)
                 .anyMatch(authorizedRoles::contains);
